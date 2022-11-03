cc03d58db52fbff56642049f24602e09b3bc12b9
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-11-03 16:17:45 +0100

    add AccessibleObject for CuteButton class

---
+++ src/WinformsControls/CuteButton.CuteButtonAccessibleObject.cs
@@ -0,0 +1,14 @@
+﻿namespace WinformsControls;
+
+public partial class CuteButton : Button
+{
+    public sealed class CuteButtonAccessibleObject : ButtonBaseAccessibleObject
+    {
+        private readonly CuteButton _owner;
+
+        public CuteButtonAccessibleObject(CuteButton owner) : base(owner)
+        {
+            _owner = owner;
+        }
+    }
+}
