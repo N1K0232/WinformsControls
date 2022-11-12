abfa07f7bb07cd12acc3f5ad25295f49fed7c7ca
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-11-12 12:19:45 +0100

    add ButtonWoc control

---
+++ src/WinformsControls/ButtonWoc.ButtonWocAccessibleObject.cs
@@ -0,0 +1,11 @@
+﻿namespace WinformsControls;
+
+public partial class ButtonWoc
+{
+    public class ButtonWocAccessibleObject : ButtonBaseAccessibleObject
+    {
+        public ButtonWocAccessibleObject(ButtonWoc owner) : base(owner)
+        {
+        }
+    }
+}
