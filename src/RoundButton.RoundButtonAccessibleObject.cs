79b1542057ee082049eda5ddebfc886b84cea019
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-12-03 09:54:33 +0100

    Add missing items to RoundButton class

---
+++ src/RoundButton.RoundButtonAccessibleObject.cs
@@ -0,0 +1,13 @@
+﻿namespace WinformsControls;
+
+public partial class RoundButton
+{
+    public class RoundButtonAccessibleObject : ButtonBaseAccessibleObject
+    {
+        public RoundButtonAccessibleObject(Control owner)
+            : base(owner is RoundButton button ? button
+            : throw new ArgumentException("invalid control", nameof(owner)))
+        {
+        }
+    }
+}
