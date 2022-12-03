9692bb5e7f51e6a20a7e45a8cf9e6753a05e7abb
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-12-03 13:51:31 +0100

    Add extension method to remove and dispose a collection of controls

--- src/Extensions/ControlCollectionExtensions.cs
+++ src/Extensions/ControlCollectionExtensions.cs
@@ -4,6 +4,27 @@ namespace WinformsControls.Extensions;
 
 public static class ControlCollectionExtensions
 {
+    /// <summary>
+    /// removes all the controls from the specified collection
+    /// </summary>
+    /// <param name="controls">the collection of controls</param>
+    public static void RemoveRange(this ControlCollection controls)
+    {
+        foreach (Control control in controls)
+        {
+            controls.Remove(control);
+            if (!control.IsDisposed)
+            {
+                control.Dispose();
+            }
+        }
+    }
+
+    /// <summary>
+    /// removes a collection of controls
+    /// </summary>
+    /// <param name="controls">the collection</param>
+    /// <param name="items">the list of items</param>
     public static void RemoveRange(this ControlCollection controls, IEnumerable<Control> items)
     {
         foreach (Control item in controls)
