bd0f91bf8a859a102931e4792fc3174bb1df6ba2
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-12-04 19:05:26 +0100

    Add summaries

--- src/CustomDateTimePicker.CustomDateTimePickerAccessibleObject.cs
+++ src/CustomDateTimePicker.CustomDateTimePickerAccessibleObject.cs
@@ -2,8 +2,17 @@
 
 public partial class CustomDateTimePicker
 {
+    /// <summary>
+    /// defines an <see cref="AccessibleObject"/> for <see cref="CustomDateTimePicker"/> control
+    /// </summary>
     public class CustomDateTimePickerAccessibleObject : DateTimePickerAccessibleObject
     {
+        /// <summary>
+        /// creates a new instance of the <see cref="CustomDateTimePickerAccessibleObject"/> class
+        /// with the specified <see cref="Control"/>
+        /// </summary>
+        /// <param name="owner">the control object</param>
+        /// <exception cref="ArgumentException">the control is not a <see cref="CustomDateTimePicker"/></exception>
         public CustomDateTimePickerAccessibleObject(Control owner)
             : base(owner is CustomDateTimePicker dateTimePicker ? dateTimePicker
                   : throw new ArgumentException("invalid control", nameof(owner)))
