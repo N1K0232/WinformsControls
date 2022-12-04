db57d17d73180638b1f81e98b0c395638fffade6
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-12-04 19:06:04 +0100

    File rename

---
+++ src/ToggleButton.ToggleButtonAccessibleObject.cs
@@ -0,0 +1,13 @@
+﻿namespace WinformsControls;
+
+public partial class ToggleButton
+{
+    public class ToggleButtonAccessibleObject : CheckBoxAccessibleObject
+    {
+        public ToggleButtonAccessibleObject(Control owner)
+            : base(owner is ToggleButton toggleButton ? toggleButton
+            : throw new ArgumentException("invalid control", nameof(owner)))
+        {
+        }
+    }
+}
